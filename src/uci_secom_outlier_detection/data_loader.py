fcd44adaba98626b43f236f7dcb9c9126403dd75
Samuel Padron <spadron05@gmail.com>
2026-09-05 20:07:23 +0200

    Move data loader to package and return dataframes

--- src/uci_secom_outlier_detection/data_loader.py
+++ src/uci_secom_outlier_detection/data_loader.py
@@ -1,18 +1,20 @@
 import os 
 import requests
 import zipfile
+import pandas as pd 
 from io import BytesIO
 from pathlib import Path
 
+
 DATA_URL = "https://archive.ics.uci.edu/static/public/179/secom.zip"
-DATA_PATH = Path('data/raw')
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+DATA_PATH = PROJECT_ROOT / "data" / "raw"
+DATA_FILE = DATA_PATH / "secom.data"
+LABELS_FILE = DATA_PATH / "secom_labels.data"
     
 
 def download_data():
-    data_file = DATA_PATH / "secom.data"
-    labels_file = DATA_PATH / "secom_labels.data"
-    
-    if data_file.exists() and labels_file.exists():
+    if DATA_FILE.exists() and LABELS_FILE.exists():
         return
 
     Path(DATA_PATH).mkdir(parents=True, exist_ok=True)
@@ -22,6 +24,12 @@ def download_data():
     r.raise_for_status()
     with zipfile.ZipFile(BytesIO(r.content)) as zip_file:
         zip_file.extractall(path=DATA_PATH)
+        
+def load_dataset():
+    download_data()
+    data_df = pd.read_csv(DATA_FILE, sep=r'\s+', header=None)
+    labels_df = pd.read_csv(LABELS_FILE, sep=r'\s+', header=None)
+    return data_df, labels_df
     
 
 
